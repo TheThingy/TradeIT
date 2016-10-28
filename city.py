7b81cd0d06c38c8568aae10e27cea14496b38f4d
Daniel A. Hardcastle <TheThing99@gmail.com>
2016-10-28 23:43:53 +0200

    update

--- city.py
+++ city.py
@@ -3,3 +3,17 @@
 class City:
     def __init__(self, city_name):
         self.city_name = city_name
+
+class CityList:
+    def __init__(self):
+        self.clist = []
+
+    def add_city(self, name):
+        self.clist.append(name)
+
+    def remove_city(self, name):
+        if name in self.clist:
+            self.clist.remove(name)
+
+    def get_list(self):
+        return self.clist
