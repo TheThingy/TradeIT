29e2b579e6f992e73af7cde848a89b8a72ef3308
Hans-Kristian <hkbruvold@gmail.com>
2016-10-28 22:49:38 +0200

    Added class for managing a product list

--- product.py
+++ product.py
@@ -6,4 +6,22 @@ class Product:
     
     def __init__(self, name):
         self.name = name
+
+
+class ProductList:
+    """ A class for managing a list of products """
+    
+    def __init__(self):
+        self.plist = []
+    
+    
+    def add_product(self, product):
+        self.plist.append(product)
+    
+    def remove_product(self, product):
+        if product in self.plist:
+            self.plist.remove(product)
+    
+    def get_list(self):
+        return self.plist
         
