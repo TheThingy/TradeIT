4fbd82c3d5676da77942b2d80294bb14b999e200
Hans-Kristian <hkbruvold@gmail.com>
2016-10-30 15:47:03 +0100

    added some fields to player

--- player.py
+++ player.py
@@ -1,3 +1,9 @@
 # ! /usr/bin/env python3
 
+import product
+
 class Player:
+    def __init__(self):
+        self.position = None
+        self.money = 0
+        self.inventory = product.ProductList()
