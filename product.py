bab15079351c8c591b6eb62999d042e6d2785e08
Hans-Kristian <hkbruvold@gmail.com>
2016-10-30 15:33:07 +0100

    Added counter for products

--- product.py
+++ product.py
@@ -18,7 +18,7 @@ class ProductList:
         if self.contains(product.name):
             return False
         
-        self.plist[product.name] = product
+        self.plist[product.name] = [product, 1] # Every product has a counter
         return True
 
     def remove_product(self, product):
@@ -32,7 +32,23 @@ class ProductList:
         return self.plist
     
     def get_product(self, name):
-        return self.plist[name]
+        if self.contains(name):
+            return self.plist[name][0]
+        
+        return False
+    
+    def get_product_amount(self, name):
+        if self.contains(name):
+            return self.plist[name][1]
+        
+        return False
+    
+    def set_product_amount(self, name, amount):
+        if self.contains(name):
+            self.plist["name"][1] = amount
+            return True
+        
+        return False
     
     def contains(self, name):
         return name in self.plist # Return True if (name in self.plist), False otherwise
