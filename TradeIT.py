3bd563c0d2e6e7d7b28cb71e4f2f259a7535c328
Hans-Kristian <hkbruvold@gmail.com>
2016-10-30 16:22:20 +0100

    moved stuff into a class

--- TradeIT.py
+++ TradeIT.py
@@ -7,25 +7,32 @@ from city import City, CityList
 from player import Player
 
 
+class Game:
+    def __init__():
+        self.product_list = ProductList()
+        self.city_list = CityList()
+        self.player = Player()
+        
+        # Load JSON files
+        for prod in json.loads(open("products.json", 'r', encoding="utf8").read()):
+            temp = Product(prod)
+            product_list.add_product(temp)
+        
+        for cty in json.loads(open("cities.json", 'r', encoding="utf8").read()):
+            temp = City(cty)
+            city_list.add_city(temp)
+        
+    
+    
+    def get_input():
+        return input(self.player.position + "$")
+    
+    def mainloop(self):
+        pass
 
-
-def main():
-    product_list = ProductList()
-    city_list = CityList()
-    player = Player()
-    
-    # Load JSON files
-    for prod in json.loads(open("products.json", 'r', encoding="utf8").read()):
-        temp = Product(prod)
-        product_list.add_product(temp)
-    
-    for cty in json.loads(open("cities.json", 'r', encoding="utf8").read()):
-        temp = City(cty)
-        city_list.add_city(temp)
-    
-    
 
 
 
 if __name__ == '__main__':
-    main()
+    game = Game()
+    game.mainloop()
