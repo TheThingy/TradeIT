d0928cf4d59b1658c4891cf681a4043c5246dc76
Daniel A. Hardcastle <TheThing99@gmail.com>
2016-10-30 20:08:08 +0100

    Added Get/Set for position, inventory and loan in player

--- player.py
+++ player.py
@@ -9,3 +9,21 @@ class Player:
         self.money = 0
         self.inventory = product.ProductList()
         self.loan = 0
+
+    def get_position(self):
+        return self.location
+
+    def set_position(self, location):
+        self.location = location
+
+    def get_inventory(self):
+        return self.inventory
+
+    def set_inventory(self, inventory):
+        self.inventory = inventory
+
+    def get_loan(self):
+        return self.loan
+
+    def set_loan(self, loan):
+        self.loan = loan
