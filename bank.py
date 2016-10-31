258a719529827f4bcc714a81539f4e80450e8697
Daniel A. Hardcastle <TheThing99@gmail.com>
2016-10-31 11:07:48 +0100

    Added get/set loan in bank

--- bank.py
+++ bank.py
@@ -4,3 +4,9 @@
 class Bank:
     def __init__(self, loan):
         self.loan = loan
+
+    def get_loan(self):
+        return self.get_loan
+
+    def set_loan(self, amount):
+        self.loan = amount
