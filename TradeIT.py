d88e6f758e2f3de629edaa65aa4061b26a36aa93
Hans-Kristian <hkbruvold@gmail.com>
2016-11-01 00:27:33 +0100

    Moved console functions to cmd.py

--- TradeIT.py
+++ TradeIT.py
@@ -6,6 +6,7 @@ import sys
 from product import Product, ProductList
 from city import City, CityList
 from player import Player
+import cmd
 
 
 class Game:
@@ -26,60 +27,12 @@ class Game:
         self.player = Player("Per", self.city_list.get_city("Tromsø"))
         
     
-    
-    def get_input(self):
-        promt = str(self.player.name)
-        promt += "@"
-        promt += str(self.player.location.city_name)
-        promt += "$ "
-        return input(promt)
-    
-    def decode_command(self, command):
-        command_list = command.split()
-        
-        if command_list[0] == "goto":
-            if len(command_list) < 2:
-                print("Error! Please specify city to go to.")
-                return
-            self.player.set_location(self.city_list.get_city(command_list[1]))
-            self.do_step()
-        if command_list[0] == "info":
-            print("Name: %s" %(self.player.name))
-            print("Money: %i" %(self.player.money))
-            print("Location: %s" %(self.player.location.city_name))
-            print("Loan: %i" %(self.player.loan))
-        elif command_list[0] == "exit":
-            print("Quitting")
-            sys.exit()
-        elif command_list[0] in ["help", "halp", "hjelp"]:
-            if len(command_list) == 1:
-                print("Available commands:")
-                print("info")
-                print("exit")
-                print("goto")
-                print("loan")
-            elif len(command_list) > 1:
-                if command_list[1] == "info":
-                    print("Usage: info")
-                    print("Shows information about your character")
-                if command_list[1] == "exit":
-                    print("Usage: exit")
-                    print("Exits the game")
-                if command_list[1] == "goto":
-                    print("Usage: goto [city]")
-                    print("Will transport the player to [city]")
-                if command_list[1] == "loan":
-                    print("Usage: loan [number]")
-                    print("Positive number borrows, negative number repays the amount")
-                    
-
     def do_step(self):
         self.step += 1
-        
 
     def mainloop(self):
         while True:
-            self.decode_command(self.get_input())
+            cmd.decode_command(self, cmd.get_input(self))
             
 
 
---
+++ cmd.py
@@ -0,0 +1,49 @@
+#! /usr/bin/env python3
+
+
+def decode_command(game, command):
+    command_list = command.split()
+    
+    if command_list[0] == "goto":
+        if len(command_list) < 2:
+            print("Error! Please specify city to go to.")
+            return
+        game.player.set_location(game.city_list.get_city(command_list[1]))
+        game.do_step()
+    elif command_list[0] == "info":
+        print("Name: %s" %(game.player.name))
+        print("Money: %i" %(game.player.money))
+        print("Location: %s" %(game.player.location.city_name))
+        print("Loan: %i" %(game.player.loan))
+    elif command_list[0] == "exit":
+        print("Quitting")
+        sys.exit()
+    elif command_list[0] in ["help", "halp", "hjelp"]:
+        if len(command_list) == 1:
+            print("Available commands:")
+            print("info")
+            print("exit")
+            print("goto")
+            print("loan")
+        else:
+            if command_list[1] == "info":
+                print("Usage: info")
+                print("Shows information about your character")
+            if command_list[1] == "exit":
+                print("Usage: exit")
+                print("Exits the game")
+            if command_list[1] == "goto":
+                print("Usage: goto [city]")
+                print("Will transport the player to [city]")
+            if command_list[1] == "loan":
+                print("Usage: loan [number]")
+                print("Positive number borrows, negative number repays the amount")
+
+
+
+def get_input(game):
+    prompt = str(game.player.name)
+    prompt += "@"
+    prompt += str(game.player.location.city_name)
+    prompt += "$ "
+    return input(prompt)
